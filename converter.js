c420a937fc4743af9047719660bbdb6bf2e18480
hawkphantomnet <hawkphantomnet@gmail.com>
2017-04-30 19:07:36 +0800

    update index.html

--- converter.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    document.querySelector("#convert").addEventListener("click",
-            function () {
-                var meters = document.querySelector("#meters");
-                var feet = document.querySelector("#feet");
-                feet.value = meters.value * 3.28084;
-            }
-            );
-};
